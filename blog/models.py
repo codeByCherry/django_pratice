456e22cf14ff9877619eece6f5e9867812068bb7
ZhangXiangxiong <2415738393@qq.com>
2018-09-05 14:13:12 +0800

    构建数据模型 BlogArticle

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class BlogArticle(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User,
+                               related_name='blogArticles',
+                               on_delete=models.CASCADE,
+                               )
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title

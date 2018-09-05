ed8acd360a82365dd9e05c708a80ca7719c33a88
ZhangXiangxiong <2415738393@qq.com>
2018-09-06 02:17:08 +0800

    add some templates files

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
 from . import views
 
 app_name = 'account'
 
 urlpatterns = [
-    path('login/', views.login, name="login"),
+    # path('login/', views.login, name="login"),
+    # path('login/', LoginView.as_view(template_name="account/login.html"), name='login'),
+    path('login/', LoginView.as_view(), name='login'),
 ]

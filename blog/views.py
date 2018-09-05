a39ba2ebdca3db09d70a3938b7c8f7700705831e
ZhangXiangxiong <2415738393@qq.com>
2018-09-05 15:17:37 +0800

    显示 blogArticle 的内容

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from .models import BlogArticle
 
@@ -10,3 +11,11 @@ def index(request):
         blogArticles=blogArticles,
     )
     return render(request, 'blog/index.html', context=context)
+
+
+def blogArticle_content(request, blogArticle_id):
+    blogArticle = get_object_or_404(BlogArticle, id=blogArticle_id)
+    context = dict(
+        blogArticle=blogArticle,
+    )
+    return render(request, 'blog/content.html', context=context)

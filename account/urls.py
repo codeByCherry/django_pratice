c7ea7697e0770874b391c2382f84a7f66bb1c8b9
ZhangXiangxiong <2415738393@qq.com>
2018-09-06 00:35:54 +0800

    完成登录页面的 UI

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'account'
+
 urlpatterns = [
-    path('', views.login, name="login"),
+    path('login/', views.login, name="login"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from .forms import LoginForm
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def login(request):
-    return HttpResponse("LOGIN")
+    login_form = LoginForm()
+    context = dict(
+        login_form=login_form,
+    )
+    return render(request, 'account/login.html', context)

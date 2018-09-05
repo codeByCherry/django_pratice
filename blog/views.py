c6964f981918da53ebd475e831e6a6c807d9a7b5
ZhangXiangxiong <2415738393@qq.com>
2018-09-05 15:01:57 +0800

    显示blogArticle 的首页

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import BlogArticle
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("HW")
+    blogArticles = BlogArticle.objects.all()
+    context = dict(
+        blogArticles=blogArticles,
+    )
+    return render(request, 'blog/index.html', context=context)

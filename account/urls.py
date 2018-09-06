856f369042d4a37c801068ed1c26e6806ea6b3d8
ZhangXiangxiong <2415738393@qq.com>
2018-09-06 11:09:13 +0800

    使用auth.views 模块下的LoginView 实现登录页面

--- account/urls.py
+++ account/urls.py
@@ -4,8 +4,15 @@ from . import views
 
 app_name = 'account'
 
+login_context = dict(
+    template_name='account/login.html',
+)
+
 urlpatterns = [
     # path('login/', views.login, name="login"),
     # path('login/', LoginView.as_view(template_name="account/login.html"), name='login'),
     path('login/', LoginView.as_view(), name='login'),
+    # path('login/', LoginView.as_view(), name='login'),
+
+    path('logout/', views.logout, name='logout'),
 ]

e0009915f93de4446133f05ad5cf88bff98442ae
ZhangXiangxiong <2415738393@qq.com>
2018-09-07 22:29:38 +0800

    新增 UserProfle 模型

--- account/models.py
+++ account/models.py
@@ -2,5 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class UserProfile():
-    pass
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
+    birth = models.DateField(null=True)
+    phone = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return "user_profile:" + self.user.username
